e7b9049175108e8e4292e84ba6f42ffb9deef665
mwehehehe <43458726+tamaasn@users.noreply.github.com>
2023-06-18 08:21:54 +0700

    Update run_ascii.py

--- run_ascii.py
+++ run_ascii.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import time
 
@@ -7,16 +6,7 @@ def run(path):
     count=0
     while count < len(ascii_dir):
         with open(path+"/frame{}.txt".format(count) , "r") as f:
-            sys.stdout.write(f.read())
+            print(f.read())
 
         time.sleep(1/30)
         count += 1
-
-    '''for ascii_file in ascii_dir:
-        filename = path+"/"+ascii_file
-        with open(filename , "r") as f:
-            sys.stdout.write(f.read())
-        time.sleep(1/30)
-        sys.stdout.flush()
-'''
-
